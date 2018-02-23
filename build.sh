e905411d9a94b690773d0d158fc41a50b610c7ac
Palash Motghare <palash.akash150@gmail.com>
2018-02-23 16:21:11 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,3 @@
+em++ -s WASM=1  multiply.cpp
+mv a.out.js multiply.js
+mv a.out.wasm multiply.wasm
---
+++ multiply.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main()
+
+{
+
+   std::cout << "initialized";
+
+}
+int multiply(float x, float y)
+
+{
+
+    return x * y;
+
+}
