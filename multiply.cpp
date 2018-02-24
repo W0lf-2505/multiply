bbdb09760b054cd322500cd72cdebb392d298e79
Palash Motghare <palash.akash150@gmail.com>
2018-02-25 01:00:59 +0530

    Step 6 done

--- multiply.cpp
+++ multiply.cpp
@@ -1,17 +1,15 @@
 #include<iostream>
 
-int main()
-
-{
+int main(){
+   
    int x,y,z;
    std::cout << "initialized\n";
 }
 
 extern "C" {
-extern int multiply_in_js(int x, int y);
-int multiply(float x, float y)
-   {
-        return multiply_in_js(x, y);
+   extern int multiply_in_js(int x, int y);
+   int multiply(float x, float y) {
+         return multiply_in_js(x, y);
 
    }
 }
